059cf0149208cee50598361621705acb4cc15171
mcraigyyy <hooklynmacy@gmail.com>
2026-09-05 15:15:08 +1000

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'workout-tracker-v15';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request)
+      .then((response) => {
+        const resClone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(e.request, resClone));
+        return response;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
